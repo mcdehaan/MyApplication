ccd65daab25f401c5bca2a613f335bf015b8cd28
Martin de Haan <m.de.haan@anva.nl>
2025-06-18 15:51:10 +0200

    Removed unused variables

--- app/src/main/java/nl/monkeysquare/myapplication/GameScreen.kt
+++ app/src/main/java/nl/monkeysquare/myapplication/GameScreen.kt
@@ -1,16 +1,15 @@
 package nl.monkeysquare.myapplication
 
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.BoxWithConstraints
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.border
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.platform.LocalConfiguration
 import androidx.compose.ui.platform.LocalDensity
 import androidx.navigation.NavHostController
 import kotlinx.coroutines.delay
@@ -25,22 +24,8 @@ import nl.monkeysquare.myapplication.objects.Snake
 
 @Composable
 fun GameScreen(navController: NavHostController, modifier: Modifier = Modifier) {
-    // Get screen dimensions dynamically
-    val configuration = LocalConfiguration.current
+    // Get the current density
     val density = LocalDensity.current
-    
-    // Convert screen dimensions from dp to pixels
-    val screenWidthPx = with(density) { configuration.screenWidthDp.dp.toPx() }
-    val screenHeightPx = with(density) { configuration.screenHeightDp.dp.toPx() }
-    
-    // Account for UI elements (status bar, navigation bar, etc.)
-    val statusBarHeight = with(density) { 24.dp.toPx() } // Approximate status bar height
-    val navigationBarHeight = with(density) { 48.dp.toPx() } // Approximate navigation bar height
-    val topBarHeight = with(density) { 56.dp.toPx() } // Height for back button and FPS counter
-    
-    // Calculate actual game area dimensions
-    val gameWidth = screenWidthPx - (2 * with(density) { 2.dp.toPx() }) // Account for border width
-    val gameHeight = screenHeightPx - statusBarHeight - navigationBarHeight - topBarHeight
     
     // Create a BoxWithConstraints to get the actual size after layout
     BoxWithConstraints(modifier = modifier.fillMaxSize()) {
